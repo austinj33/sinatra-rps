06ab10f6bb573e92049bf991fef5d7f931cef1d3
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-04-10 02:40:21 +0000

    added_rock

--- app.rb
+++ app.rb
@@ -7,3 +7,20 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "rock"
+    outcome = "We Tied!"
+  elsif comp_move == "scissors"
+    outcome = "You won!"
+  else comp_move == "paper"
+    outcome = "You lost!"
+  end
+  
+erb(:zebra)
+
+end
